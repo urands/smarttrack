c3802c7108dec79bf70888eb106ed900921c0554
urands <ds@inbox.ru>
2014-08-05 03:20:50 +0400

    MCU startup project

---
+++ mcu/RTE/RTE_Components.h
@@ -0,0 +1,45 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'smarttrack' 
+ * Target:  'Debug' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_ADC
+#define RTE_DEVICE_STDPERIPH_BKP
+#define RTE_DEVICE_STDPERIPH_CAN
+#define RTE_DEVICE_STDPERIPH_CEC
+#define RTE_DEVICE_STDPERIPH_CRC
+#define RTE_DEVICE_STDPERIPH_DAC
+#define RTE_DEVICE_STDPERIPH_DBGMCU
+#define RTE_DEVICE_STDPERIPH_DMA
+#define RTE_DEVICE_STDPERIPH_EXTI
+#define RTE_DEVICE_STDPERIPH_FLASH
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_IWDG
+#define RTE_DEVICE_STDPERIPH_PWR
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_RTC
+#define RTE_DEVICE_STDPERIPH_SPI
+#define RTE_DEVICE_STDPERIPH_TIM
+#define RTE_DEVICE_STDPERIPH_USART
+#define RTE_DEVICE_STDPERIPH_WWDG
+#define RTE_Drivers_I2C1                /* Driver I2C1 */
+        #define RTE_Drivers_I2C2                /* Driver I2C2 */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+#define RTE_Drivers_UART1               /* Driver UART1 */
+        #define RTE_Drivers_UART2               /* Driver UART2 */
+        #define RTE_Drivers_UART3               /* Driver UART3 */
+        #define RTE_Drivers_UART4               /* Driver UART4 */
+        #define RTE_Drivers_UART5               /* Driver UART5 */
+#define RTE_Drivers_USBH0               /* Driver USBH0 */
+
+#endif /* RTE_COMPONENTS_H */
---
+++ mcu/core/main.c
@@ -0,0 +1,19 @@
+
+#include "stm32f10x_conf.h"
+#include <stdio.h>
+#include <stm32f10x.h>
+
+int main(void){
+	
+	//printf("Start SMARTTRACKER programm\n");
+	
+
+
+	return 0;
+}
+
+
+ void assert_failed(uint8_t* file, uint32_t line){
+	 //printf("assert_failed\n");
+ }
+
